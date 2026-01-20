eff55a190ec614383f42acf44f96ba9149efa778
llll2005 <f20050510@gmail.com>
2026-01-20 09:06:10 +0800

    hw4 finish

---
+++ assignments/Homework4/build/CMakeFiles/BezierCurve.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BezierCurve.dir/link.d"
+  "BezierCurve"
+  "BezierCurve.pdb"
+  "CMakeFiles/BezierCurve.dir/main.cpp.o"
+  "CMakeFiles/BezierCurve.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BezierCurve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
